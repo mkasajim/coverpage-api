1047ce26a0056bb264dc1a14a4e25d5c472ae171
Md Kawsar Ali <63280801+mkasajim@users.noreply.github.com>
2023-02-12 20:28:07 +0000

    Ch

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import os
 
 from coverpage_generator import *
 
@@ -36,5 +37,5 @@ def create_cover(id: str, course_code: str, course_title: str, teacher_name: str
 
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get('PORT', 17995))
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    PORT = int(os.environ.get('PORT', 3000))
+    uvicorn.run(app, host="127.0.0.1", port=PORT)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import os
 # import uuid
 import shortuuid
 
