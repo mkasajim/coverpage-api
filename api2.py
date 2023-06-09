05d7a3dd10feb30b1ebb570effc02faae7574f00
Md Kawsar Ali <63280801+mkasajim@users.noreply.github.com>
2023-06-09 01:28:22 +0000

    Mobile preview

--- api2.py
+++ api2.py
@@ -327,8 +327,8 @@ async def preview():
     return """
     <html>
         <head>
-            <title>Generate Coverpage</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Coverpage Preview</title>
+            
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
             <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
